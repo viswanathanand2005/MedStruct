1311ec19824db9a2bb5155f4f2177e2fa1b28646
CH Viswanath Anand <viswanathanand2005@gmail.com>
2026-04-09 10:20:04 +0530

    Completed stage-3

--- stage-3/run_stage-3.py
+++ stage-3/run_stage-3.py
@@ -9,13 +9,13 @@ def main():
     os.makedirs(output_dir, exist_ok=True)
 
     print("Loading data...")
-    df_entities = pd.read_csv(input_entities)
+    df_entities = pd.read_csv(input_entities).head(2000)
     df_sentences = pd.read_csv(input_sentences)
 
     extractor = RelationExtractor(
         drug_labels=['DRUG'], 
         disease_labels=['problem'],
-        device=-1 # Set to 0 if you have a GPU
+        device=-1
     )
 
     df_relations = extractor.extract_drug_disease(df_entities, df_sentences, window=1, threshold=0.7)
@@ -23,7 +23,7 @@ def main():
     output_file = os.path.join(output_dir, "relations_verified.csv")
     df_relations.to_csv(output_file, index=False)
     
-    print(f"🎉 Stage 3 Complete! Verified {len(df_relations)} semantic relationships.")
+    print(f"Stage 3 Complete! Verified {len(df_relations)} semantic relationships.")
 
 if __name__ == "__main__":
     main()
